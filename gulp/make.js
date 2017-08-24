570210bea6c0002452526ec9e37fb4c87a0f13ca
Zainabed <zainabed@gmail.com>
2017-08-24 12:46:18 +0530

    Chanaged process of building component.

--- gulp/make.js
+++ gulp/make.js
@@ -9,13 +9,14 @@ function make(){
     files.forEach(function(file){
       let basePath = path.dirname(file);
       let fileName = path.basename(file, '.js');
-      let cssFile = path.join(basePath, fileName + '.style.html');
+      let cssFile = path.join(basePath, fileName + '.css');
       let templateFile = path.join(basePath, fileName + '.tpl.html');
-      let componentFile = path.join(basePath, fileName + '.cp.html');
+      let componentFile = path.join(fileConfig.build.path, fileName + '.cp.html');
     //  console.log(cssFile);
      // console.log(templateFile);
       fs.readFile(file, 'utf8', function(error, scriptContent){
         fs.readFile(cssFile, 'utf8', function(error, cssContent){
+          cssContent = '<style>' + cssContent + '</style>';
           fs.readFile(templateFile, 'utf8', function(error, templateContent){
             let content = cssContent + templateContent;
             content = '<script>' + scriptContent.replace('<%conent%>', content) + '</script>';
---
+++ gulp/sass.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+var glob = require('glob');
+var fileConfig = require('../config/fileConfig.js');
+var path= require('path');
+var sass = require('node-sass');
+var gulp = require('gulp');
+
+gulp.task('sass', function(){
+  return buildSass();
+})
+
+function buildSass(){
+  glob(fileConfig.component.sass, function(error, files) {
+    files.forEach(function(file){
+      let basePath = path.dirname(file);
+      let fileName = path.basename(file, '.scss');
+      if(fileName.charAt(0) !== '_'){
+        sass.render({
+          file: file
+        }, function(error, content){
+
+          if(error) {
+            console.log(error);
+            return;
+          }
+          if(content.css){
+            let cssFile = path.join(basePath, fileName + '.css');
+
+            fs.writeFile(cssFile, content.css , function(error){
+              console.log(error);
+            })
+          }else{
+            console.log('Failed to compile scss '+ fileName +' file.');
+          }
+
+        });
+      }
+
+    })
+  });
+}
